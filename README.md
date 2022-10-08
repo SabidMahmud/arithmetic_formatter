2361ee1573fab8c6cd66bd1cc23769fe80614f90
SabidMahmud <badg3r@pop-os.localdomain>
2022-10-09 01:13:43 +0600

    First commit in this repository

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# arithmetic_formatter
-A project for freecodecamp.org Scientific Computing with Python.
+# Arithmetic Formatter
+
+This is the boilerplate for the Arithmetic Formatter project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/arithmetic-formatter
---
+++ arithmetic_arranger.py
@@ -0,0 +1,26 @@
+# import re
+
+def arithmetic_arranger(problems, expect_solution = False):
+    if len(problems) > 5:
+        return "Error: Too many problems."
+
+    upper_numbers = ''
+    lower_numbers = ''
+    operator = ''
+    dash = ''
+    answer = ''
+    for problem in problems:
+        upper_numbers = problem.split(" ")[0].strip()
+        operator = problem.split(" ")[1].strip()
+        lower_numbers = problem.split(' ')[2].strip()
+
+        if (upper_numbers.isdigit() or lower_numbers.isdigit()) == False:
+            return "Error: Numbers must only contain digits."
+            
+        # if (operator != '+') or (operator != '-'):
+        #     return "Error: Operator must be '+' or '-'."
+        
+        if ( len(upper_numbers) or len(lower_numbers) ) >= 5 :
+            return "Error: Numbers cannot be more than four digits."
+        
+print(arithmetic_arranger(["3q2 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
